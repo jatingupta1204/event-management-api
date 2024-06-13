6f1d170363d1556c993172b0a3db0470e193ddcb
Jatin <jatingupta1204@gmail.com>
2024-06-13 16:34:46 +0530

    feat: added some utils

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express from "express"
-import CookieParser from "cookieparser"
+import cors from "cors"
+import CookieParser from "cookie-parser"
 
 const app = express()
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(CookieParser())
